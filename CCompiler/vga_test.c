ff5ad85fb397ed85aba385e12178e26012377785
Nicolas <73864341+FISHARMNIC@users.noreply.github.com>
2022-05-05 22:28:49 -0700

    Add files via upload

---
+++ CCompiler/vga_test.c
@@ -0,0 +1,37 @@
+char *ttyPosition = 0xB8000;
+char exampleString[] = "chicken";
+
+int BGcolor = 0; // BLACK
+int FGcolor = 15; // WHITE
+
+void putchar(int character)
+{
+    *ttyPosition = character;
+    ttyPosition = eq(ttyPosition + 1);   
+    *ttyPosition = eq(BGcolor << 2 | FGcolor);
+    ttyPosition = eq(ttyPosition + 1); 
+}
+
+void putstring(int *string)
+{
+    while(*string != 0) {
+        putchar(*string);
+        string = eq(string + 1);
+    }
+}
+
+void putint(int number)
+{
+    // BROKEN
+    while(number > 0) {
+        putchar(eq(number % 10 + 48));
+        number = eq(number / 10);
+        halt();
+    }
+}
+
+int main() {
+    putchar('A');
+    putstring(&exampleString);
+    return(0);
+}
